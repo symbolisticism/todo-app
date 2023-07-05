7ef7570fae90ae31cd879960d320a611fcad02f1
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-05 15:54:58 -0700

    added dummy data to work with until I have basic functionality down

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,10 @@
+import 'package:todo/models/todo_item.dart';
+
+final dummyData = [
+  TodoItem(id: '1', done: false, description: 'first item'),
+  TodoItem(id: '2', done: false, description: 'second item'),
+  TodoItem(id: '3', done: false, description: 'third item'),
+  TodoItem(id: '4', done: false, description: 'fourth item'),
+  TodoItem(id: '5', done: false, description: 'fifth item'),
+  TodoItem(id: '6', done: false, description: 'sixth item'),
+];
