4a3986dac7ec84f49a9e7f41a9e5e244556af42b
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-05 15:55:23 -0700

    defined a basic todo item as a model

---
+++ lib/models/todo_item.dart
@@ -0,0 +1,11 @@
+class TodoItem {
+  TodoItem({
+    required this.id,
+    required this.done,
+    required this.description,
+  });
+
+  final String id;
+  bool done;
+  final String description;
+}
