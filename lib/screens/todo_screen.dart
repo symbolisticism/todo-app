94376cedf364c30a8edea9b9a1eb3986245115a3
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-05 17:21:35 -0700

    added bottom overlay

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:todo/data/dummy_data.dart';
+import 'package:todo/widgets/new_item.dart';
 import 'package:todo/widgets/todo_list.dart';
 
 class Todo extends StatelessWidget {
   const Todo({super.key});
+
+  void _showBottomOverlay(BuildContext context) {
+    showModalBottomSheet(
+      useSafeArea: true,
+      isScrollControlled: true,
+      context: context,
+      builder: (context) => const NewItem(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +22,7 @@ class Todo extends StatelessWidget {
         title: const Text('To Do List'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => _showBottomOverlay(context),
             icon: const Icon(Icons.add),
           ),
         ],
