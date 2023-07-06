14b65529a196e2597b3e3c967e86daf12cb4d8ff
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-06 13:39:50 -0700

    added validateInput, addItem, and showDialog functions

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo/data/dummy_data.dart';
+import 'package:todo/models/todo_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -8,11 +10,51 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
-  final _formKey = GlobalKey<FormState>();
+  
   final _descriptionController = TextEditingController();
   String? _enteredDescription;
 
-  void _addItem() {}
+  void _addItem(String description) {
+    dummyData.add(
+      TodoItem(
+          id: DateTime.now().toString(), done: false, description: description),
+    );
+
+    for (final item in dummyData) {
+      print(DateTime.now());
+      print(item.description);
+    }
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: const Text('Invalid input'),
+        content: const Text('Please enter a description.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Okay'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool _validateInput() {
+    _enteredDescription = _descriptionController.text.trim();
+    final inputIsValid = _enteredDescription != null &&
+        _enteredDescription!
+            .isNotEmpty; // checked for null value in the first condition
+
+    if (inputIsValid) return true;
+
+    _showDialog();
+    return false;
+  }
 
   @override
   void dispose() {
@@ -22,37 +64,43 @@ class _NewItemState extends State<NewItem> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
+    // this property is necessary for making sure the keyboard
+    // doesn't cover the input field
+    final keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
+    return Padding(
+      padding: EdgeInsets.fromLTRB(16, 16, 16, keyboardSpace + 16),
+      child: SizedBox(
+        // this makes the overlay take up most of the screen
+        height: double.infinity,
+        child: Column(
+          children: [
+            TextField(
+              controller: _descriptionController,
+              decoration: const InputDecoration(
+                labelText: 'Description',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Row(
-                  children: [
-                    TextFormField(
-                      maxLength: 25,
-                      decoration: const InputDecoration(
-                        label: Text('Description'),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Description must be 1 or more characters long.';
-                        }
-                        return null;
-                      },
-                      onSaved: (newValue) {
-                        _enteredDescription = newValue;
-                      },
-                    )
-                  ],
-                )
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Cancel'),
+                ),
+                const SizedBox(width: 24),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Add Item'),
+                ),
               ],
             ),
-          ),
-        );
-      },
+          ],
+        ),
+      ),
     );
   }
 }
