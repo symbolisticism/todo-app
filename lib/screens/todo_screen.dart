2562c321584581ea3d29efda5980419c345737ef
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-11 14:51:18 -0700

    removed unused import

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:todo/data/dummy_data.dart';
 import 'package:todo/widgets/new_item.dart';
 import 'package:todo/widgets/todo_list.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
